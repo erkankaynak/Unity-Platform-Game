d07c9eb352dbd7089597710233a3c83387782221
erkan kaynak <erkan.kaynak.001@gmail.com>
2022-11-03 14:33:43 +0300

    Su dalgası

--- Assets/WaterScript.cs
+++ Assets/WaterScript.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class WaterScript : MonoBehaviour
 {
     
-    public float speed = 3f;
-    private float timer = 3f;
-    private Vector3 direction = Vector3.forward;
+    public float speed = 1f;
+    private float timer = 2f;
+    private Vector3 direction = Vector3.up;
 
     // Start is called before the first frame update
     void Start()
